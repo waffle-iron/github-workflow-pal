a2d5c7ab93370f80251c76a2e3a00be54a5973f4
Gabriele Petronella <gabriele@buildo.io>
2016-01-04 15:42:05 +0100

    Move jquery helper functions to enableActivePlaceholders

--- src/plugin/enableActivePlaceholders.js
+++ src/plugin/enableActivePlaceholders.js
@@ -1,3 +1,35 @@
+import $ from 'jquery';
+
+$.fn.selectRange = function(start, end) {
+  return this.each(function() {
+    if(this.setSelectionRange) {
+      this.focus();
+      this.setSelectionRange(start, end);
+    } else if(this.createTextRange) {
+      const range = this.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', end);
+      range.moveStart('character', start);
+      range.select();
+    }
+  });
+};
+
+$.fn.getCursorPosition = function() {
+  const el = $(this).get(0);
+  let pos = 0;
+  if('selectionStart' in el) {
+    pos = el.selectionStart;
+  } else if('selection' in document) {
+    el.focus();
+    const sel = document.selection.createRange();
+    const selLength = document.selection.createRange().text.length;
+    sel.moveStart('character', -el.value.length);
+    pos = sel.text.length - selLength;
+  }
+  return pos;
+};
+
 export default function enableActivePlaceholders(issueBody) {
   issueBody.click(function() {
     if (window.getSelection().toString().length) {

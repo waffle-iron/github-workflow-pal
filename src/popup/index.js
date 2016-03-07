e5903a65369ea52ce469c0f7017872e5fd25bc51
Francesco <firefelix@gmail.com>
2016-03-07 21:50:16 +0100

    add popup logic (so far only gyazo token)

--- src/popup/index.js
+++ src/popup/index.js
@@ -2,3 +2,32 @@
 require('../icons/icon16.png');
 require('../icons/icon48.png');
 require('../icons/icon128.png');
+
+const getElements = () => ({
+  form: document.getElementById('form'),
+  gyazoToken: document.getElementById('gyazo_token')
+});
+
+const update = () => {
+  const { gyazoToken } = getElements();
+
+  chrome.storage.sync.get({ accessToken: '' }, (items) => {
+    gyazoToken.value = items.accessToken;
+  })
+};
+
+const save = (cb) => {
+  const { gyazoToken } = getElements();
+  chrome.storage.sync.set({
+    accessToken: gyazoToken.value
+  }, cb);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  update();
+
+  getElements().form.onsubmit = (e) => {
+    e.preventDefault();
+    save(update);
+  };
+});
